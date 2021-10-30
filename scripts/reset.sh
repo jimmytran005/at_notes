3f2775bba17af687bbd97b8e3bd5bf1243cac206
jimmytran005 <jimmy.tran005@umb.edu>
2021-10-29 21:39:13 -0400

    removed scripts

--- scripts/reset.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name "jimmytran16"
-git config --global user.email "jimmytran1620@gmail.com"
--- scripts/set.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name "jimmytran005"
-git config --global user.email "jimmy.tran005@umb.edu"
