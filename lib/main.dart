e1ba820d4f670f5ae9c4fd2a1f2dd19fe1740266
jimmytran005 <jimmy.tran005@umb.edu>
2021-10-30 13:51:54 -0400

    fixed comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'screens/settings.dart';
 
-// entry point function
+// entry point function, will call the MyApp() to start the app
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Named Routes Demo',
       // Start the app with the "/" named route. In this case, the app starts
-      // on the FirstScreen widget.
+      // on the HomePage widget.
       initialRoute: '/',
       routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
+        // When navigating to the "/" route, build the HomePage widget.
         '/': (context) => HomePage(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
+        // When navigating to the "/second" route, build the SettingsPage widget.
         '/settings': (context) => SettingsPage(),
       },
     );
