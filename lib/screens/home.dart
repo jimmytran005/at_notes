d75ee0a0ca835d13e57ece2631be197b1240f724
jimmytran16 <jimmytran1620@gmail.com>
2021-11-09 22:40:49 -0500

    added scrollable for notes, made button floatable

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,58 +7,64 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
-      body: Column(
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            Note(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
-                'Feb 10, 2020',
-                false),
-            Note(
-                'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit',
-                'Feb 10, 2020',
-                false)
-          ]),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            Note(
-                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
-                'Feb 10, 2020',
-                true),
-          ]),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            Note(
-                'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.',
-                'Feb 10, 2020',
-                false),
-            Note(
-                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
-                'Feb 10, 2020',
-                false)
-          ]),
-          Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  // REFERENCE : https://api.flutter.dev/flutter/material/IconButton-class.html
-                  Ink(
-                    decoration: const ShapeDecoration(
-                      color: Colors.lightBlue,
-                      shape: CircleBorder(),
-                    ),
-                    child: IconButton(
-                      icon: const Icon(Icons.add),
-                      color: Colors.white,
-                      // when the button is pressed, we want to navigate to the note widget
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/note');
-                      },
-                    ),
-                  )
-                ],
-              ))
-        ],
+      // SingleChildScrollView : REFERENCE : https://api.flutter.dev/flutter/widgets/SingleChildScrollView-class.html
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Note(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                  'Feb 10, 2020',
+                  false),
+              Note(
+                  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit',
+                  'Feb 10, 2020',
+                  false)
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Note(
+                  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit',
+                  'Feb 10, 2020',
+                  true),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Note(
+                  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.',
+                  'Feb 10, 2020',
+                  false),
+              Note(
+                  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+                  'Feb 10, 2020',
+                  false)
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Note(
+                  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.',
+                  'Feb 10, 2020',
+                  false),
+              Note(
+                  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+                  'Feb 10, 2020',
+                  false)
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Note(
+                  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.',
+                  'Feb 10, 2020',
+                  false),
+              Note(
+                  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+                  'Feb 10, 2020',
+                  false)
+            ]),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/note');
+          },
+          child: const Icon(Icons.add)),
     );
   }
 }
